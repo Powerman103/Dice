73428eab6337be1e4b143d8f31b374f5dd1b3e1b
Powerman103 <31930972+Powerman103@users.noreply.github.com>
2021-07-11 16:19:17 -0400

    Add files via upload

---
+++ game.py
@@ -0,0 +1,28 @@
+from random import randint
+
+score = 0
+
+i = 0
+while i < 40:
+    i += 1
+    dice_number = randint(1,6)
+    number_chosen = int(input("Which number from 1-6 will you guess?"))
+
+    if dice_number == number_chosen:
+        print("Good job guessing it right!")
+        score += 10
+        
+    else:
+        print("The dice number was", dice_number, "you imbicile")
+        
+
+
+print("Good job on your score of", score, "!")
+
+
+
+
+ 
+
+        
+        
